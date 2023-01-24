4dd561013e388412c678e37ba1ca9907975886c5
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-01-24 15:34:09 -0400

    projects adde

--- components/ProjectPreview.tsx
+++ components/ProjectPreview.tsx
@@ -1,42 +1,40 @@
-// deno-lint-ignore-file no-explicit-any
 import IconChevronRight from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/chevron-right.tsx";
 import BrandGithub from "https://deno.land/x/tabler_icons_tsx@0.0.1/tsx/brand-github.tsx";
 
 type Props = {
-  title?: string,
-  overview?: any,
-  releaseDate?: string,
-  poster?: {
-    asset: {
-      _ref: string
-    }
-  }
+  title: string,
+  description: string,
+  github: string,
+  view: string,
+  picture: string
 }
 
-export default function ProjectPreview({ title, overview, releaseDate, poster }: Props) {
+export default function ProjectPreview({ title, description, github, view, picture }: Props) {
   return (
     <div
       class="w-full flex px-8 h-96 justify-center items-center flex-col gap-8 bg-cover bg-center bg-no-repeat bg-gray-100 rounded-xl text-white"
-      style="background-image:linear-gradient(rgba(0, 0, 40, 0.8),rgba(0, 0, 40, 0.8)), url('/hero-bg.webp');"
+      style={`background-image:linear-gradient(rgba(0, 0, 40, 0.9),rgba(0, 0, 40, 0.95)), url('${picture}');`}
     >
       <div class="space-y-4 text-center">
-        <h1 class="text-4xl inline-block font-bold">Project Component Beta</h1>
+        <h1 class="text-4xl inline-block font-bold">{title}</h1>
         <p class="text-xl max-w-lg text-blue-100">
-          This is a Project made by me, click bellow to access the project site or it's Github repository
+          {description}
         </p>
       </div>
 
       <div>
         <a
-          href="#"
+          href={github}
           class="block mt-4 text-blue-500 cursor-pointer inline-flex items-center group text-blue-800 bg-white px-8 py-2 rounded-md hover:bg-blue-50 font-bold"
+          target="github"
         >
           GitHub{" "}
           <BrandGithub class="ml-2" />
         </a>
         <a
-          href="#"
+          href={view}
           class="block mt-4 transition-colors text-blue-400 cursor-pointer inline-flex items-center group px-4 py-2 hover:text-blue-100"
+          target={title}
         >
           View Project{" "}
           <IconChevronRight class="inline-block w-5 h-5 transition group-hover:translate-x-0.5" />
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,11 +6,47 @@ type Props = {
 }
 
 export default function Movies({ movies }: Props) {
-    const index = [[],[],[],[],[]]
+    const projects = [
+        {
+            title: "Madames",
+            description: "M'adames is a Brazilian physical and online store that sells products focused on women's intimate life.",
+            github: "https://github.com/adeirjunior/madames",
+            view: "https://madames.store/",
+            picture: "/projects/madames.png",
+        },
+        {
+            title: "Assuntos",
+            description: "A blog made with the aim of helping me to complete my programming studies, where I write about content that I am studying at the moment.",
+            github: "https://github.com/adeirjunior/assuntos",
+            view: "https://assuntos.dev/",
+            picture: "/projects/assuntos.png",
+        },
+        {
+            title: "Bitcoin Conversor",
+            description: "Bitcoin to Dollar and Euro Converter.",
+            github: "https://github.com/adeirjunior/bitcoin-price-tracker",
+            view: "https://bpt.deno.dev/",
+            picture: "/projects/btc.png",
+        },
+        {
+            title: "Hulu Clone",
+            description: "Clone of the Hulu Streaming service, where the most famous movies of today are shown, categorized into Action, Comedy, Romance, Animation, Horror, and so on.",
+            github: "https://github.com/adeirjunior/hulu-clone",
+            view: "https://hulu-clone-lake-eight.vercel.app/",
+            picture: "/projects/hulu.png",
+        },
+        {
+            title: "ScandiWeb",
+            description: "Inventory manager project for a store created during the selection process of an Estonian company called ScandiWeb",
+            github: "https://github.com/adeirjunior/scandiweb-test-task",
+            view: "https://scandiwebtesttasksite.000webhostapp.com/",
+            picture: "/projects/scandiweb.png",
+        },
+    ]
 
     return (
         <div class="grid gap-8 grid-cols-1 sm:grid-cols-2">
-            {index.map((index, key) => (<ProjectPreview key={key} />))}
+            {projects.map((project, key) => (<ProjectPreview key={key} {...project} />))}
         </div>
     )
 
