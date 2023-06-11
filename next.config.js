d24344e286da79c3029f0f716a2a51a3195050c6
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-06-10 23:32:23 -0400

    secure system applyed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,35 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';
+`;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path",
+        headers: [
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
 
 const withPWA = require("next-pwa")({
   dest: "public",
