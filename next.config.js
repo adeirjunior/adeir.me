3d1f8b444757e7669c556dfaaecf08b63d7c138c
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-06-13 00:51:59 -0400

    secure content removed

--- next.config.js
+++ next.config.js
@@ -1,37 +1,7 @@
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self';
-  child-src example.com;
-  style-src 'self' example.com;
-  font-src 'self';
-`;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path",
-        headers: [
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
-const withPWA = require("next-pwa")({
+const withPWA = require("@imbios/next-pwa")({
   dest: "public",
 });
 
