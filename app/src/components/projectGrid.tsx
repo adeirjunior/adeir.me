d8c7468af0147f28c41a5f3c7633b5fe505561f2
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-06-20 13:27:59 -0400

    project grid margin fix

--- app/src/components/projectGrid.tsx
+++ app/src/components/projectGrid.tsx
@@ -8,7 +8,7 @@ type Prop = {
 function projectGrid({projects}: Prop) {
   return (
     <section className="text-gray-400 dark:bg-gray-900 body-font">
-      <div className="container px-5 py-24 mx-auto">
+      <div className="mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {projects.map((project, index) => (
             <ProjectCard project={project} key={index}/>
