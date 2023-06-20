e77081edc75df4cdb5d32a1bb03efcfe46a9f510
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-06-19 23:51:06 -0400

    footer link fix

--- app/src/components/footer.tsx
+++ app/src/components/footer.tsx
@@ -23,27 +23,23 @@ export default async function Footer({ lang }: any) {
           <div className="font-bold dark:text-white text-base">{item.title}</div>
           <ul className="mt-2 text-base">
             {item.children.map((child) => {
-              if (child.name === "Blog") {
-                return (
-                  <li className="mt-2" key={child.name}>
-                    <Link
-                      className="text-gray-500 hover:text-gray-700 dark:text-gray-200 dark:hover:text-gray-400"
-                      href={child.href}
-                      target={child.name}
-                    >
-                      {child.name}
-                    </Link>
-                  </li>
-                );
-              }
               return (
                 <li className="mt-2" key={child.name}>
-                  <a
+                  {item.title === 'Menu' & child.name !== 'Blog' ? (
+                    <Link
                     className="text-gray-500 hover:text-gray-700 dark:text-gray-200 dark:hover:text-gray-400"
                     href={child.href}
                   >
                     {child.name}
-                  </a>
+                  </Link>
+                  ) : (<a
+                    className="text-gray-500 hover:text-gray-700 dark:text-gray-200 dark:hover:text-gray-400"
+                    href={child.href}
+                    target={child.name}
+                  >
+                    {child.name}
+                  </a>)
+                  }
                 </li>
               );
             })}
