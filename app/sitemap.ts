5124c167af1b60cbb5cd2d11df3b8d16dac9b7c4
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-09-02 12:20:49 -0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,26 @@
+import loadGithubRepositories from "./(utils)/load-github-repositories";
 import { getBlogPosts } from "./(utils)/utils"
 
 export const baseUrl = process.env.BASE_URL as string
 
 export default async function sitemap() {
   const data = await getBlogPosts()
+  const projectsData = await loadGithubRepositories();
 
   const blogs = data.map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
+    url: `${baseUrl}/blog/${post.slug.current}`,
     lastModified: post.publishedAt,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+   const projects = projectsData.map((project) => ({
+     url: `${baseUrl}/projects/${project.name}`,
+     lastModified: new Date().toISOString().split("T")[0],
+   }));
+
+  const routes = ['', '/blog', '/projects', '/links'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogs]
+  return [...routes, ...blogs, ...projects];
 }
