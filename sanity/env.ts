80e16e9e2f84003081a4644d886a55250761b48c
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-09-02 10:59:24 -0400

    Update env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-09-02'
 
 export const dataset = assertValue(
-  process.env.SANITY_DATASET,
+  process.env.SANITY_STUDIO_DATASET,
   "Missing environment variable: SANITY_DATASET"
 );
 
 export const projectId = assertValue(
-  process.env.SANITY_ID,
+  process.env.SANITY_STUDIO_ID,
   "Missing environment variable: SANITY_ID"
 );
 
