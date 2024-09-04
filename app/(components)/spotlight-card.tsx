92a03e5d9323602cd471d365e8d3cc4362ceb9e0
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-09-04 00:39:26 -0400

    Update spotlight-card.tsx

--- app/(components)/spotlight-card.tsx
+++ app/(components)/spotlight-card.tsx
@@ -86,30 +86,31 @@ export function SpotlightCard({
 }: SpotlightCardProps) {
   return (
     <div
-      className={`relative h-full bg-slate-800 rounded-3xl p-px before:absolute before:w-80 before:h-80 before:-left-40 before:-top-40 before:bg-slate-400 before:rounded-full before:opacity-0 before:pointer-events-none before:transition-opacity before:duration-500 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:group-hover:opacity-100 before:z-10 before:blur-[100px] after:absolute after:w-96 after:h-96 after:-left-48 after:-top-48 after:bg-indigo-500 after:rounded-full after:opacity-0 after:pointer-events-none after:transition-opacity after:duration-500 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:hover:opacity-10 after:z-30 after:blur-[100px] overflow-hidden ${className}`}
+      className={`relative h-full dark:bg-slate-800 bg-slate-200 dark:text-slate-200 text-slate-800 rounded-3xl p-px before:absolute before:w-80 before:h-80 before:-left-40 before:-top-40 before:dark:bg-slate-400 before:bg-slate-600 before:rounded-full before:opacity-0 before:pointer-events-none before:transition-opacity before:duration-500 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:group-hover:opacity-100 before:z-10 before:blur-[100px] after:absolute after:w-96 after:h-96 after:-left-48 after:-top-48 after:dark:bg-indigo-500 after:bg-indigo-300 after:rounded-full after:opacity-0 after:pointer-events-none after:transition-opacity after:duration-500 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:hover:opacity-10 after:z-30 after:blur-[100px] overflow-hidden ${className}`}
     >
       {children}
     </div>
   );
 }
 
-export function CustomSpotlightCard({children}: SpotlightCardProps) {
+export function CustomSpotlightCard({ children }: SpotlightCardProps) {
   return (
-    
-      <SpotlightCard>
-        <div className="relative h-full bg-black p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
-          <div
-            className="absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/2 aspect-square"
-            aria-hidden="true"
-          >
-            <div className="absolute inset-0 translate-z-0 bg-slate-800 rounded-full blur-[80px]"></div>
-          </div>
-          <div className="flex flex-col h-full items-center text-center">
-            <div className="grow mb-5">
-              <h3 className="text-xl text-slate-200 font-bold mb-1">{children}</h3>
-            </div>
+    <SpotlightCard>
+      <div className="relative h-full dark:bg-black bg-white dark:text-slate-200 text-slate-800 p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
+        <div
+          className="absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/2 aspect-square"
+          aria-hidden="true"
+        >
+          <div className="absolute inset-0 translate-z-0 dark:bg-slate-800 bg-slate-200 rounded-full blur-[80px]"></div>
+        </div>
+        <div className="flex flex-col h-full items-center text-center">
+          <div className="grow mb-5">
+            <h3 className="text-xl dark:text-slate-200 text-slate-800 font-bold mb-1">
+              {children}
+            </h3>
           </div>
         </div>
-      </SpotlightCard>
+      </div>
+    </SpotlightCard>
   );
 }
