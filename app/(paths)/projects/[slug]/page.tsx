18152544a9c8957904c6464a5aac394be3dc9960
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-09-28 22:12:29 -0400

    Update page.tsx

--- app/(paths)/projects/[slug]/page.tsx
+++ app/(paths)/projects/[slug]/page.tsx
@@ -60,7 +60,7 @@ async function page({ params }: Props) {
     notFound();
   }
 
-  const { name, description, readme } = repository;
+  const { name, description, readme, url } = repository;
 
   const sanitizedReadme = sanitizeMDXContent(readme);
 
@@ -84,9 +84,15 @@ async function page({ params }: Props) {
           }),
         }}
       />
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
-        {params.slug}
-      </h1>
+      <div className="flex justify-between">
+        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+          {params.slug}
+        </h1>
+        <a target="_blank" href={url}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide hover:opacity-80 lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
+        </a>
+      </div>
+
 
       <article className="prose">
         <CustomMDX source={sanitizedReadme} />
