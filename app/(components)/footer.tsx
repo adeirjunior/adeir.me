108e2674367ef4df33ad7bd33e45a819f2ed4a53
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-10-05 13:53:24 -0400

    Update footer.tsx

--- app/(components)/footer.tsx
+++ app/(components)/footer.tsx
@@ -37,9 +37,9 @@ export default function Footer() {
   }
 
   return (
-    <footer className="mb-6 noprint">
+    <footer className="mb-6 ">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
+        <li className="noprint">
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -62,7 +62,7 @@ export default function Footer() {
           </a>
         </li>
       </ul>
-      <div className="flex justify-between items-center mt-8">
+      <div className="flex justify-between items-center mt-8 noprint">
         <p className="text-neutral-600 dark:text-neutral-300">
           © {new Date().getFullYear()} MIT Licensed
         </p>
