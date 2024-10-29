98bbb96702a09e50d6ed423c5b99d0c327493b32
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-10-29 12:18:38 -0400

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -21,9 +21,9 @@ export default async function page({ params }: { params: { lang: 'en' | 'pt' } }
             <section className="prose">
                 <h2>{home.programming_languages.title}</h2>
                 <ul>
-                    <li>Rust</li>
-                    <li>Typescript</li>
-                    <li>Kotlin</li>
+                    <li>Rust <span>(C++)</span></li>
+                    <li>Typescript <span>(Javascript)</span></li>
+                    <li>Kotlin <span>(Java)</span></li>
                 </ul>
             </section>
 
